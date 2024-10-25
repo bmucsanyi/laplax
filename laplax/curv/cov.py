WIP: Fix in full covariance and naming.

laplax/curv/cov.py
@@ -4,7 +4,8 @@ import jax
 import jax.numpy as jnp
 
 from laplax.curv.low_rank import get_low_rank, inv_low_rank_plus_diagonal_mv_factory
-from laplax.types import Callable
+from laplax.types import Callable, PyTree
+from laplax.util.flatten import create_partial_pytree_flattener
 from laplax.util.mv import array_to_mv, diagonal, todense
 
 # -----------------------------------------------------------------------
@@ -29,16 +30,22 @@ def prec_to_scale(prec: jax.Array) -> jax.Array:
     return L
 
 
-def full_cov_factory(mv: Callable, size: int):
+def create_full_cov(mv: Callable, tree: PyTree):
+    """Create posterior covariance function for full curvature estimate.
+
+    For inverting we flatten the PyTree and apply unflattening after.
+    """
     # Get dense curvature estimate
-    curv_est = todense(mv, size=size)
+    curv_est = todense(mv, like=tree)
+    flatten, unflatten = create_partial_pytree_flattener(curv_est)
+    curv_est = flatten(curv_est)
 
     def get_posterior(prior_prec, return_scale=False):
-        prec = curv_est + prior_prec * jnp.eye(size)
+        prec = curv_est + prior_prec * jnp.eye(curv_est.shape[-1])
         scale = prec_to_scale(prec)
         if return_scale:
-            return array_to_mv(scale)
-        return array_to_mv(scale @ scale.T)
+            return array_to_mv(unflatten(scale))
+        return array_to_mv(unflatten(scale @ scale.T))
 
     return get_posterior
 
@@ -48,7 +55,7 @@ def full_cov_factory(mv: Callable, size: int):
 # -----------------------------------------------------------------------
 
 
-def diagonal_cov_factory(mv: Callable, size: int):
+def create_diagonal_cov(mv: Callable, size: int):
     # Get diagonal curvature estimate
     curv_diagonal = diagonal(mv, size=size)
 
@@ -65,7 +72,7 @@ def diagonal_cov_factory(mv: Callable, size: int):
 # -----------------------------------------------------------------------
 
 
-def low_rank_cov_factory(mv: Callable, size: int, **kwargs):
+def create_low_rank_cov(mv: Callable, size: int, **kwargs):
     # Get low rank terms
     low_rank_terms = get_low_rank(mv, size, **kwargs)
 