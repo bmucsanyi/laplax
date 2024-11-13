477f106b1ffd8598b04fd0cf6c4f2a67722a18f0
L-Kasu <lars@kasueschke.de>
2024-11-13 11:56:41 +0100

    Push forward tes parametrezied

--- tests/test_eval/test_push_forward.py
+++ tests/test_eval/test_push_forward.py
@@ -8,44 +8,97 @@ from laplax.eval.push_forward import set_mc_pushforward
 from laplax.util.flatten import create_partial_pytree_flattener
 from laplax.util.mv import todense
 from laplax.util.tree import allclose, get_size, ones_like
+from flax import linen as nn
+import equinox as eqx
+from pytest_cases import parametrize_with_cases
 
-
-def load_simple_model():
-    class Model(nnx.Module):
-        def __init__(self, in_channels, hidden_channels, out_channels, rngs):
-            self.in_channels = in_channels
-            self.hidden_channels = hidden_channels
-            self.out_channels = out_channels
-
-            self.linear1 = nnx.Linear(in_channels, hidden_channels, rngs=rngs)
-            self.linear2 = nnx.Linear(hidden_channels, hidden_channels, rngs=rngs)
-            self.linear3 = nnx.Linear(hidden_channels, out_channels, rngs=rngs)
+# use for one of: "linen", "nnx" or "equinox
+def case_linen():
+    class MLP(nn.Module):
+        def setup(self, in_channels=1, hidden_channels=20, out_channels=1):
+            # Define the layers in the init method
+            self.linear1 = nn.Dense(hidden_channels)
+            self.linear2 = nn.Dense(out_channels)
 
         def __call__(self, x):
+            # Apply the layers to the input x
             x = self.linear1(x)
-            x = nnx.relu(x)
+            x = nn.tanh(x)
             x = self.linear2(x)
-            x = nnx.relu(x)
-            x = self.linear3(x)
             return x
 
     key = jax.random.key(0)
     rngs = nnx.Rngs(0)
-    model = Model(in_channels=10, hidden_channels=20, out_channels=1, rngs=rngs)
     data = {
         "input": jax.random.normal(key, (1, 10)),
         "target": jax.random.normal(key, (1, 1)),
     }
-    return model, data
+
+    model = MLP(in_channels=10, hidden_channels=20, out_channels=1)
+
+    model_fn = lambda params, x: model.apply(params, input)
+    graph_def, _ = nnx.split(model)
+    return model, data, model_fn
 
 
-def test_push_forward():
-    # Create simple model
-    model, data = load_simple_model()
-    graph_def, params = nnx.split(model)
+def case_nnx():
+    class MLP(nnx.Module):
+        def __init__(self, rngs, in_channels=1, hidden_channels=20, out_channels=1):
+            self.linear1 = nnx.Linear(in_channels, hidden_channels, rngs=rngs)
+            self.tanh = nnx.tanh
+            self.linear2 = nnx.Linear(hidden_channels, out_channels, rngs=rngs)
+
+        def __call__(self, x):
+            x = self.linear1(x)
+            x = self.tanh(x)
+            x = self.linear2(x)
+            return x
+
+    key = jax.random.key(0)
+    rngs = nnx.Rngs(0)
+    data = {
+        "input": jax.random.normal(key, (1, 10)),
+        "target": jax.random.normal(key, (1, 1)),
+    }
+
+    model = MLP(in_channels=10, hidden_channels=20, out_channels=1, rngs=rngs)
+    graph_def, _ = nnx.split(model)
 
     def model_fn(params, input):
         return nnx.call((graph_def, params))(input)[0]
+
+    return model, data, model_fn
+
+"""
+def case_equinox():
+    class MLP(eqx.Module):
+        layers: list
+
+        def __init__(self, rngs, in_channels=1, hidden_channels=20, out_channels=1):
+            self.layers = [eqx.nn.Linear(in_channels, hidden_channels, rngs),
+                           eqx.nn.Linear(hidden_channels, out_channels, rngs]
+
+        def __call__(self, x):
+            for layer in self.layers[:-1]:
+                x = jax.nn.tanh(layer(x))
+            return self.layers[-1](x)
+
+    key = jax.random.key(0)
+    rngs = nnx.Rngs(0)
+    data = {
+        "input": jax.random.normal(key, (1, 10)),
+        "target": jax.random.normal(key, (1, 1)),
+    }
+
+    model = MLP(in_channels=10, hidden_channels=20, out_channels=1, rngs=rngs)
+    return model, data
+"""
+
+
+@parametrize_with_cases("model, data, model_fn", cases='.')
+def test_push_forward(model, data, model_fn):
+    graph_def, params, rest = nnx.split(model, nnx.Param, ...)
+
 
     # Calculate ggn
     ggn_mv = create_ggn_mv(model_fn, params, data, "mse")
@@ -69,8 +122,7 @@ def test_push_forward():
     # Compute pushforward
     results = pushforward(data["input"])
 
-    jnp.allclose(model_fn(params, data["input"]), results["pred"])
-    jnp.allclose(model_fn(params, data["input"]), results["pred_mean"], rtol=1e-2)
+    assert jnp.allclose(model_fn(params, data["input"]), results["pred"])
+    assert jnp.allclose(model_fn(params, data["input"]), results["pred_mean"], rtol=1e-2)
 
 
-test_push_forward()
