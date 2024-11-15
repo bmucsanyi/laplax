2cb2b9acd3aa8d81aeaae409c030629bff1caa63
L-Kasu <lars@kasueschke.de>
2024-11-15 13:24:15 +0100

    classification nnx added

--- tests/test_eval/test_push_forward.py
+++ tests/test_eval/test_push_forward.py
@@ -9,6 +9,8 @@ from laplax.eval.push_forward import set_mc_pushforward
 from laplax.util.flatten import create_partial_pytree_flattener
 from laplax.util.mv import todense
 from laplax.util.tree import allclose, get_size, ones_like
+from functools import partial
+
 from flax import linen as nn
 import equinox as eqx
 from pytest_cases import parametrize_with_cases
@@ -16,7 +18,7 @@ from jax import random
 
 #TODO: als class, klasse case function mit inchannels etc. paranmetrisieren möglich
 
-class Regression():
+class Regression:
 # use for one of: "linen", "nnx" or "equinox
     def case_linen(self):
         class MLP(nn.Module):
@@ -115,65 +117,45 @@ class Regression():
         params = eqx.filter(model, eqx.is_inexact_array)
         return model_fn, data, params
 
-
-"""
-class Classification():
+class Classification:
 # use for one of: "linen", "nnx" or "equinox
-    def case_linen(self):
-        class MLP(nn.Module):
-            def setup(self, in_channels=1, hidden_channels=20, out_channels=1):
-                # Define the layers in the init method
-                self.linear1 = nn.Dense(hidden_channels)
-                self.linear2 = nn.Dense(out_channels)
-
-            def __call__(self, x):
-                # Apply the layers to the input x
-                x = self.linear1(x)
-                x = nn.tanh(x)
-                x = self.linear2(x)
-                return x
-
-        key = jax.random.key(0)
-        rngs = nnx.Rngs(0)
-        data = {
-            "input": jax.random.normal(key, (1, 10)),
-            "target": jax.random.normal(key, (1, 1)),
-        }
-
-        model = MLP(in_channels=10, hidden_channels=20, out_channels=1)
-
-        model_fn = lambda params, x: model.apply(params, input)
-        graph_def, _ = nnx.split(model)
-        return model, data, model_fn
 
     def case_nnx(self):
-        class MLP(nnx.Module):
-            def __init__(self, rngs, in_channels=1, hidden_channels=20, out_channels=1):
-                self.linear1 = nnx.Linear(in_channels, hidden_channels, rngs=rngs)
-                self.tanh = nnx.tanh
-                self.linear2 = nnx.Linear(hidden_channels, out_channels, rngs=rngs)
+        class CNN(nnx.Module):
+            """A simple CNN model."""
+
+            def __init__(self, rngs: nnx.Rngs):
+                self.conv1 = nnx.Conv(1, 2, kernel_size=(3, 3), rngs=rngs)
+                self.avg_pool = partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2))
+                self.linear1 = nnx.Linear(32, 10, rngs=rngs)
 
             def __call__(self, x):
-                x = self.linear1(x)
-                x = self.tanh(x)
-                x = self.linear2(x)
+                if x.ndim == 3:
+                    x = jnp.expand_dims(x, axis=0)
+                x = self.avg_pool(nnx.relu(self.conv1(x)))
+                x = x.reshape(x.shape[0], -1)  # flatten
+                x = nnx.relu(self.linear1(x))
                 return x
 
         key = jax.random.key(0)
         rngs = nnx.Rngs(0)
         data = {
-            "input": jax.random.normal(key, (1, 10)),
-            "target": jax.random.normal(key, (1, 1)),
+            # Single grayscale image with shape (1, 8, 8, 1) for batch size 1
+            "input": jax.random.normal(key, (1, 8, 8, 1)),
+
+            # Random one-hot target with 10 classes
+            "target": jax.nn.one_hot(jax.random.randint(key, (1,), 0, 10), num_classes=10),
         }
 
-        model = MLP(in_channels=10, hidden_channels=20, out_channels=1, rngs=rngs)
-        graph_def, parameters = nnx.split(model)
+        model = CNN(rngs=rngs)
 
         def model_fn(params, input):
-            return nnx.call((graph_def, params))(input)[0]
+            nnx.update(model, nnx.GraphState.merge(params, rest))  # Load the model from parameters
+            return model(input)
 
-        return model, data, model_fn, parameters
-
+        _, params, rest = nnx.split(model, nnx.Param, ...)
+        return model_fn, data, params
+"""
     def case_equinox(self):
         class MLP(eqx.Module):
             layers: list
@@ -196,8 +178,9 @@ class Classification():
 
         model = MLP(in_channels=10, hidden_channels=20, out_channels=1, rngs=rngs)
         return model, data
+        
 """
-@parametrize_with_cases("model_fn, data, params", cases=[Regression])
+@parametrize_with_cases("model_fn, data, params", cases=[Classification])
 def test_push_forward(model_fn, data, params):
     # Calculate ggn
     ggn_mv = create_ggn_mv(model_fn, params, data, "mse")
