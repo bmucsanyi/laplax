0cd292c204d5e7ebe6fc63aeba7b9e3ccaacf014
2bys <tobias.weber.hh@gmail.com>
2024-12-02 19:23:56 +0100

    ADD: Prob predictive on databatch wo metrics.

--- laplax/eval/__init__.py
+++ laplax/eval/__init__.py
@@ -2,11 +2,12 @@
 
 from .calibrate import evaluate_for_given_prior_arguments, optimize_prior_prec
 from .push_forward import set_lin_pushforward, set_mc_pushforward
-from .utils import evaluate_metrics_on_dataset
+from .utils import evaluate_metrics_on_dataset, evaluate_on_dataset
 
 __all__ = [
     "evaluate_for_given_prior_arguments",
     "evaluate_metrics_on_dataset",
+    "evaluate_on_dataset",
     "optimize_prior_prec",
     "set_lin_pushforward",
     "set_mc_pushforward",
--- laplax/eval/utils.py
+++ laplax/eval/utils.py
@@ -19,6 +19,25 @@ def finalize_functions(functions: OrderedDict, results: dict, **kwargs):
     return results
 
 
+def evaluate_on_dataset(pred_fn: Callable, data: tuple[jax.Array], **kwargs) -> dict:
+    """Evaluate prob_predictive on dataset.
+
+    Args:
+        pred_fn: A callable that takes an input and returns predictions.
+        data: A tuple of input and target data.
+        metircs: A list of callable metrics or a single callable metric.
+        **kwargs:
+            - lmap_eval: lmap batchsize
+    Returns:
+        A dictionary containing predictions.
+    """
+
+    def evaluate_data_point(dp: tuple[jax.Array]) -> dict:
+        return {**pred_fn(dp["input"]), "target": dp["target"]}
+
+    return lmap(evaluate_data_point, data, batch_size=kwargs.get("lmap_eval", "data"))
+
+
 def evaluate_metrics_on_dataset(  # noqa: D417
     pred_fn: Callable,
     data: tuple[jax.Array],
